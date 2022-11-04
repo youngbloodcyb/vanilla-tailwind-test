3bdcb326b1b84de720f103ce5a09771691d2bd12
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-04 10:20:39 -0600

    install express, update tailwind dir

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("this is working");
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('index.html');
+});
+
+app.listen(3030, (err) => {
+    if (err) console.log(err);
+    console.log("Server Running on port 3030");
+});
